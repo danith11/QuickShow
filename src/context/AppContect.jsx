87ab05e118d491f49cfca629d62f3a05a27403d2
Yethum Danith <yethumdanith2002@gmail.com>
2025-08-24 13:57:20 +0530

    Fetch Admin App Context

--- src/context/AppContect.jsx
+++ src/context/AppContect.jsx
@@ -1,10 +1,42 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { createContext } from "react";
+import axios from "axios";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useLocation, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ cildren }) => {
-  const value = {};
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [shows, setShows] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+  const { user } = useUser();
+  const { getToken } = useAuth();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const fetchIsAdmin = async () => {
+    try {
+      const { data } = await axios.get("/api/admin/is-admin", {
+        headers: { Authorization: `Bearer ${await getToken()}` },
+      });
+
+      setIsAdmin(data.isAdmin);
+
+      if (!data.isAdmin && location.pathname.startsWith("/admin")) {
+        navigate("/");
+        toast.error("You are not authorized to access admin dashboard");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const value = { axios };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
