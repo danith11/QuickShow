947357fa6bdedf6f5377184ca8315e658bd5842f
Yethum Danith <yethumdanith2002@gmail.com>
2025-08-29 14:11:57 +0530

    Fetching data from backend

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { dummyShowsData } from "../assets/assets";
 import MovieCard from "../components/MovieCard";
 import BlurCircle from "../components/BlurCircle";
+import { useAppContext } from "../context/AppContext";
 
 const Favorite = () => {
-  return dummyShowsData.length > 0 ? (
+  const { favoriteMovies } = useAppContext();
+  return favoriteMovies.length > 0 ? (
     <div className="overflow-hidden relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44">
       <BlurCircle top="150px" left="0" />
       <BlurCircle bottom="50px" right="50px" />
       <h1 className="my-4 text-lg font-medium">Your Favorite Movies</h1>
       {/* <div className="grid grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 items-center gap-4"> */}
       <div className="flex flex-wrap gap-8 max-sm:justify-center">
-        {dummyShowsData.map((movie) => (
+        {favoriteMovies.map((movie) => (
           <MovieCard movie={movie} key={movie._id} />
         ))}
       </div>
@@ -21,6 +22,6 @@ const Favorite = () => {
       <h1 className="text-3xl font-bold text-center">No Movies Available</h1>
     </div>
   );
-}
+};
 
-export default Favorite
+export default Favorite;
--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { dummyShowsData } from "../assets/assets";
 import MovieCard from "../components/MovieCard";
 import BlurCircle from "../components/BlurCircle";
+import { useAppContext } from "../context/AppContext";
 
 const Movies = () => {
-  return dummyShowsData.length > 0 ? (
+  const { shows } = useAppContext();
+  return shows.length > 0 ? (
     <div className="overflow-hidden relative my-40 mb-60 px-6 md:px-16 lg:px-40 xl:px-44">
       <BlurCircle top="150px" left="0" />
       <BlurCircle bottom="50px" right="50px" />
       <h1 className="my-4 text-lg font-medium">Now Showing</h1>
       {/* <div className="grid grid-cols-4 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 items-center gap-4"> */}
       <div className="flex flex-wrap gap-8 max-sm:justify-center">
-        {dummyShowsData.map((movie) => (
+        {shows.map((movie) => (
           <MovieCard movie={movie} key={movie._id} />
         ))}
       </div>
